c5b1741c809f5528755229385b9279569f142f0a
NakataNoGyaku <demertknight@gmail.com>
2019-01-23 19:10:31 +0900

    Basic flocking behavior implemented,
    #Alignment is applied
    #Cohesion is applied
    #Separation is applied.

---
+++ Assets/_SCRIPTS/Flocking/FlockBehaviour.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockBehaviour : MonoBehaviour
+{
+    public float transVel; //Velocity of the Translation
+    private float rotVel; //Velocity of the Rotation
+    Vector3 avgHeading; 
+    Vector3 avgPos;
+    private float neighbourDist; //Distance to decide if your neighbour enters the sub-group;
+
+    void Start()
+    {
+        transVel = Random.Range(.5f, 1.0f);
+        rotVel = 4.0f;
+        neighbourDist = 3.0f;
+    }
+
+    void Update()
+    {
+        if(Random.Range(0, 5) < 1)
+        {
+            ApplyRules();
+        }
+        this.transform.Translate(.0f, .0f, Time.deltaTime * transVel);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] mBirds;
+        mBirds = FlockManager.allBirds;
+
+        Vector3 mCenterVector = Vector3.zero;
+        Vector3 mAvoidVector = Vector3.zero;
+
+        int mGroupSize = 0; //Amount of birds within the sub-group within the main flock.
+        float mGroupVel = .1f; //Velocity of the sub-group.
+
+        Vector3 mGoalPos = FlockManager.goalPos;
+
+        float mDist;
+        
+        foreach (GameObject mBird in mBirds)
+        {
+            if(mBird != this.gameObject)
+            {
+                mDist = Vector3.Distance(this.transform.position, mBird.transform.position);
+                if(mDist <= neighbourDist)
+                {
+                    mCenterVector += mBird.transform.position;
+                    mGroupSize++;
+
+                    if(mDist < 1.0f)
+                    {
+                        mAvoidVector = mAvoidVector + (this.transform.position - mBird.transform.position);
+                    }
+
+                    FlockBehaviour mOtherFlockBehaviour = mBird.GetComponent<FlockBehaviour>();
+                    mGroupVel = mGroupVel + mOtherFlockBehaviour.transVel;
+                }
+            }
+        }
+
+        if(mGroupSize > 0)
+        {
+            mCenterVector = mCenterVector/mGroupSize + (mGoalPos - this.transform.position);
+            transVel = mGroupVel/mGroupSize;
+
+            Vector3 mDirection = (mCenterVector + mAvoidVector) - this.transform.position;
+            if(mDirection != Vector3.zero)
+            {
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                                           Quaternion.LookRotation(mDirection),
+                                                           rotVel * Time.deltaTime);
+            } 
+        }
+
+    }
+}
---
+++ Assets/_SCRIPTS/Flocking/FlockManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject birdObj;  //Prefab of the Bird
+    static int birdAmount = 10; //Initial Amount of birds
+    public static GameObject[] allBirds = new GameObject[birdAmount]; //List containing the Birds
+    
+    public static int spawnRange = 5;   //Range radius for spawning the birds.
+    public static Vector3 goalPos = Vector3.zero; //Goal position for the birds to flock towards.
+    
+    void Start()
+    {
+        for (int i = 0; i < birdAmount; i++)
+        {
+            Vector3 pos = new Vector3(  Random.Range(-spawnRange, spawnRange),
+                                        Random.Range(-spawnRange, spawnRange),
+                                        Random.Range(-spawnRange, spawnRange));
+            allBirds[i] = (GameObject) Instantiate(birdObj, pos, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        if(Random.Range(1, 1000) < 50)
+        {
+            goalPos = new Vector3(  Random.Range(-spawnRange, spawnRange),
+                                    Random.Range(-spawnRange, spawnRange),
+                                    Random.Range(-spawnRange, spawnRange));
+        }
+    }
+}
